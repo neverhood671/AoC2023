058db41a9802da9736214b7d699c47cb21d7a03b
Anastasiia Mishunina <nastya-art@yandex.ru>
2023-12-06 09:58:04 +0100

    add day6 solutions

---
+++ day6/solution-part1.js
@@ -0,0 +1,25 @@
+const readFileToString = require('../utils/readFileToString');
+const writeStringToFile = require('../utils/writeStringToFile');
+const input = readFileToString('input.txt');
+const getArrOfNumbersFromStr = require('../utils/getArrOfNumbersFromStr');
+
+const calcNumberOfVariants = (times, distances) => {
+  const attempts = times.map((time, i) => {
+    const currRecord = distances[i];
+    let res = 0;
+    for (let t = 0; t < time; t++) {
+      if ((time - t) * t > currRecord) res++;
+    }
+    return res;
+  });
+  return attempts.reduce((acc, curr) => acc * curr, 1);
+};
+const getAnswer = strings => {
+  const times = getArrOfNumbersFromStr(strings[0]),
+    distances = getArrOfNumbersFromStr(strings[1]);
+
+  return calcNumberOfVariants(times, distances);
+};
+writeStringToFile(getAnswer(input).toString(), 'answer1.txt');
+
+module.exports.calcNumberOfVariants = calcNumberOfVariants;
---
+++ day6/solution-part2.js
@@ -0,0 +1,13 @@
+const readFileToString = require('../utils/readFileToString');
+const writeStringToFile = require('../utils/writeStringToFile');
+const input = readFileToString('input.txt');
+const getArrOfNumbersFromStr = require('../utils/getArrOfNumbersFromStr');
+const {calcNumberOfVariants} = require('./solution-part1');
+
+const getAnswer = strings => {
+  const times = getArrOfNumbersFromStr(strings[0].replaceAll(' ', '')),
+    distances = getArrOfNumbersFromStr(strings[1].replaceAll(' ', ''));
+
+  return calcNumberOfVariants(times, distances);
+};
+writeStringToFile(getAnswer(input).toString(), 'answer2.txt');
